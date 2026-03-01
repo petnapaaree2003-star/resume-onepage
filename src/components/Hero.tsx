c088e50db0132b446c0f60b755de4d711cdf49e9
petnapaaree2003-star <petnapa.aree2003@gmail.com>
2026-03-01 19:59:05 +0700

    [feature] litle update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,18 @@
 function Hero() {
-  return (
-    <section className="text-center px-10 pt-16 pb-10 border-b">
-      <h1 className="text-4xl md:text-5xl font-light tracking-[0.3em] mb-6">
-        PETNAPA AREE
-      </h1>
+    return (
+        <section className="text-center px-10 pt-16 pb-10 border-b">
+            <h1 className="text-4xl md:text-5xl font-light tracking-[0.3em] mb-6">
+                PETNAPA AREE
+            </h1>
 
-      <p className="text-gray-700 leading-relaxed max-w-3xl mx-auto text-sm md:text-base">
-        Independently developed a One-Page Resume web application using React,
-        Vite, and TypeScript, implementing a well-structured component-based
-        architecture. Designed and organized the project in alignment with
-        modern frontend engineering standards and deployed to production
-        through a CI/CD workflow.
-      </p>
-    </section>
-  );
+            <p className="text-gray-700 leading-relaxed max-w-3xl mx-auto text-sm md:text-base">
+                Independently developed a One-Page Resume web application using React, Vite, and TypeScript, implementing a well-structured component-based architecture.
+                Designed and organized the project in alignment with modern frontend engineering standards, incorporating Git for systematic version control and branch management.
+                Configured and deployed the application to a production environment through a CI/CD workflow to enable efficient and reliable updates.
+                Demonstrated strong technical discipline, clean code practices, and readiness for professional Frontend Development environments.
+            </p>
+        </section>
+    );
 }
 
 export default Hero;
